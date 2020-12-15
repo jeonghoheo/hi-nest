8341927aaea9bf6feb8a228be94473a6781b23a2
jeonghoheo <heojeongho1992@gmail.com>
2020-12-15 20:08:20 +0900

    add - DTOs and Validation part Two

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 @Controller('movies')
@@ -35,7 +36,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     this.moviesService.update(movieId, updateData);
   }
 }
